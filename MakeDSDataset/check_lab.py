93a6a7dee5e0752b9f7b8095880a70e85c59e580
komisteng <2356991516@qq.com>
2024-02-15 22:07:26 +0800

    完成音频长度检测

--- MakeDSDataset/check_lab.py
+++ MakeDSDataset/check_lab.py
@@ -1,5 +1,6 @@
 import click
 import os
+from pydub import AudioSegment
 
 @click.command()
 @click.option('--path', help='source folder')
@@ -11,32 +12,36 @@ def progress(path, dictionary):
     if not os.path.isfile(dictionary):
         print(f'Dictionary {dictionary} does not exist')
         return
-    
-    print(path,dictionary)
-
 
     files = os.listdir(path)
     
-    # 创建两个字典，分别存储.wav和.lab文件的文件名（不包含扩展名）
     wav_files = {os.path.splitext(file)[0] for file in files if file.endswith('.wav')}
     lab_files = {os.path.splitext(file)[0] for file in files if file.endswith('.lab')}
     
-    # 检查.wav文件是否有对应的.lab文件
-    has_error = False
+    lab_has_error = False
     for wav_base in wav_files:
         if wav_base not in lab_files:
-            print(f"Error: {wav_base}.wav does not have a corresponding .lab file.")
-            has_error = True
+            print(f"错误：{wav_base}.wav 没有对应的 .lab 文件。Error: {wav_base}.wav does not have a corresponding .lab file.")
+            lab_has_error = True
     
-    # 检查.lab文件是否有对应的.wav文件
     for lab_base in lab_files:
         if lab_base not in wav_files:
-            print(f"Error: {lab_base}.lab does not have a corresponding .wav file.")
-            has_error = True
+            print(f"错误：{lab_base}.lab 没有对应的 .wav 文件。Error: {lab_base}.lab does not have a corresponding .wav file.")
+            lab_has_error = True
 
-    # 如果没有错误，打印成功提示
-    if not has_error:
+    length_has_error = False
+    for wav_file in wav_files:
+        wav_path = os.path.join(path, f"{wav_file}.wav")
+        audio = AudioSegment.from_wav(wav_path)
+        if audio.duration_seconds > 25:
+            print(f"警告：{wav_file}.wav 过长。Warning: {wav_file}.wav is too long.")
+            length_has_error = True
+    if length_has_error:
+        print("过长的音频可能会造成训练时显存溢出。Too long audio files may cause CUDA Out Of Memory.")
+
+    if not lab_has_error and not length_has_error:
         print("All files are paired successfully.")
+
 
 
 if __name__ == '__main__':
